14ce1d540c8126f2bc379e6e875a4a02a8664512
djjolicoeur <djjolicoeur@gmail.com>
2013-09-28 09:47:41 -0400

    doc

--- src/wedding_day_shennanigans/core.clj
+++ src/wedding_day_shennanigans/core.clj
@@ -2,13 +2,17 @@
 
 
 (defn val-of-string 
+    "recursively sum the ASCII values of the chars 
+     that make up the passed in string and store 
+     the value in acc."  
     [st acc] 
     (if 
       (empty? st) acc 
       (recur (rest st) (+ acc (int (first st))))))
 
 (defn -main[]
-  (println (val-of-string "djcs" 0)))
+  "For shits and giggles, the sum of the ASCII values or our initials."
+  (println "Sum of ASCII string: " (val-of-string "djcs" 0)))
 
 
 
